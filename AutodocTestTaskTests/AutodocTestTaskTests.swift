3d41b7ccb1b2df13fcefc41cabf026511cbf2c80
Andrei Yarmukhametov <andyarus@gmail.com>
2026-06-10 02:36:02 +0500

    AutodocTestTask created

---
+++ AutodocTestTaskTests/AutodocTestTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutodocTestTaskTests.swift
+//  AutodocTestTaskTests
+//
+//  Created by Andrei Yarmukhametov on 10.06.2026.
+//
+
+import Testing
+@testable import AutodocTestTask
+
+struct AutodocTestTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
